1d5558ff63eafc92f1adf645e4450dc65b7e3517
maheemalik550 <maheemalik550@gmail.com>
2023-09-18 16:26:56 +0500

    ready to commit

---
+++ app.js
@@ -0,0 +1,118 @@
+//Q 2
+//  var student = studentName = {};
+// console.log(student);
+// Q 1
+// student = [];
+// console.log(student);
+// Q 3
+
+// arr = ["maheen"];
+// console.log(arr);
+// Q 4
+// arr =[5];
+// console.log(arr);
+// Q 5
+// arr = [false];
+// console.log(arr);
+
+// Q 6
+// var arr = ["canada", 12 , true , "America"];
+// console.log(arr);
+// Q 7
+//  var degree = [ "SSC"  + "<br>" + "HSC " + "<br>" + "BSC" + "<br>" + "BCOM"+ "<br>" + "MS" + "<br>" + "M.Phill" +  "<br>" +"PhD" ];
+//  document.write(degree);
+
+
+// Q 8 skip
+
+// Q 9 
+//  var color = ["red", "yellow","black", "white","purple", "blue",];
+//  document.write(color);
+// a)
+
+// var userColor = prompt("enter your color");
+// var arr = ["red", "blue", "white" ];
+// arr.unshift(userColor);
+// console.log(arr);
+//  b)
+// var userImput = prompt("enter your fav color");
+// var arr = ["red","blue" ,"white"];
+// arr.push(userImput);
+// document.write(arr);
+// c)
+
+// var color1 = prompt("enter color");
+// var color2 = prompt("enter color");
+// var arr = ["white", "red" , "yellow", "purple"];
+// arr.unshift(color1,color2);
+// document.write(arr);
+
+// d)
+// color = ["black","blue","purple","red","yellow"];
+// color.shift();
+// document.write(color);
+// e)
+// color = ["black","blue","purple","red","yellow"];
+// color.pop();
+// document.write(color);
+
+// f)
+// var color = ["white","blue","green","black"];
+// var userImput = prompt("enter color");
+// color.unshift(userImput);
+// document.write(color);
+
+// g)
+// var color = ["red","blue","black","green","white"];
+// console.log("original", color);
+// var targetValue = prompt("enter target value");
+// var deletdValue = prompt("enter deleted value");
+
+// color.splice(targetValue,deletdValue);
+// console.log("updated",color);
+
+
+// Q 10 skip
+// Q 11
+// var cityName = ["karachi","lahore","Islamabad","Quetta","Peshawar"];
+// console.log("original array", cityName);
+
+// var StartingValue = prompt("enter StartingValue");
+// var EndingValue = prompt("enter endingValue");
+// var copy = cityName.splice(StartingValue,EndingValue);
+// console.log("updated", copy);
+
+// Q 12
+
+// var word = ["This", "is", "my", "cat"];
+// console.log(word.join(' '));
+
+
+
+// Q 13
+
+// arr = [];
+
+// arr.push("keyboard");
+// arr.push("mouse");
+// arr.push("printer");
+// arr.push("monitor");
+// console.log(arr);
+
+// Q 14
+// arr = [];
+// arr.unshift("keyboard");
+// arr.unshift("mouse");
+// arr.unshift("printer");
+// arr.unshift("monitor");
+// console.log(arr);
+
+// Q 15
+// var arr = ["Samsung","Motorola","Nokia","vivo","Sony"];
+// document.write("<select>" + "<option>" + arr[0] + "</option>" +
+// "<option>" + arr[1] + "</option>"  +
+// "<option>" + arr[2] + "</option>"  +
+// "<option>" + arr[3] + "</option>"  +
+// "<option>" + arr[4] + "</option>"  +
+// "<option>" + arr[5] + "</option>"  +
+// "</select>")
